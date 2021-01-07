a4977c91f1097a8ee507e629b0de8e506941869e
Galina-Blokh <galinablokh0307@gmail.com>
2021-01-07 02:17:34 +0200

    added chromdriver created 3 function to collect all website links

--- main.py
+++ main.py
@@ -1,69 +1,89 @@
+from selenium import webdriver
 import time
+from selenium.common.exceptions import NoSuchElementException
 
-
-def get_links_from_page(my_webpage):
-    """
-    get links for competitions from one page
-    :param my_webpage: link to page for scrapping
-    :return: links from one page
-    """
-    competition_links = []
-    for i in range(1, 20):
-        try:
-            compet = my_webpage.find_element_by_xpath(
-                '//*[@id="root"]/div/div[1]/div[2]/div/div/div[2]/div[2]/div[2]/a[' + str(i) + ']')
-        except:
-            # print('not now')
-            continue
-        competition_links.append(compet.get_attribute("href"))
-    return competition_links
+URL = f"https://www.simplyrecipes.com/latest/"
+FILE_NAME = 'recipe_links_simplyrecipes'
 
 
 def connect():
     """
     create chrome driver
-    :return: chrome driver
+    :return: chrome driver:obj
     """
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     driver = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
-    driver.get("https://www.kaggle.com/search?q=in%3Acompetitions")
+    driver.get(URL)
     time.sleep(5)
     return driver
 
 
-def get_links_from_site(driver, num_pages=450):
+def get_links_from_one_page(my_webpage):
     """
-    get competition links from kaggle site
-    :param driver: chrome driver
-    :param num_pages: number of pages to scrap
+    To collect links for each recipe from one page
+    :param my_webpage: link to page for scrapping: selenium obj
+    :return: links from one page: list[str]
+    """
+    recipe_links = []
+    # we know that on this website we have 26 links per page
+    for i in range(1, 27):
+        try:
+            recipe = my_webpage.find_element_by_xpath(
+                f'//*[@id="page"]/div[2]/div/div/div/div/div/ul/li[{i}]/div/div[2]/h2/a')
+        except:
+            continue
+        recipe_links.append(recipe.get_attribute("href"))
+    return recipe_links
+
+
+def get_links_from_site(recipe_driver, num_pages=132):
+    """
+    To list pages on website and collect links into list
+    :param recipe_driver: obj  chrome driver
+    :param num_pages: int number of pages to scrap / default 132
     :return: list of links
     """
-    competition_links = []
-    for i in range(num_pages):
-        competition_links += get_links_from_page(driver)
-        driver.find_element_by_xpath(
-            '//*[@id="root"]/div/div[1]/div[2]/div/div/div[2]/div[2]/div[3]/div/button[2]').click()
-        time.sleep(5)
-    return competition_links
+    all_pages_links = []
+    for i in range(1, num_pages):
+
+        # get links from one page
+        page = get_links_from_one_page(recipe_driver)
+        all_pages_links.append(page)
+
+        try:
+            # go to next page
+            recipe_driver.find_element_by_class_name('rpg-next').click()
+            time.sleep(5)
+            print(f'page {i} is collected')
+        except NoSuchElementException:
+            print('I guess the page have no more recipes')
+            continue
+    return all_pages_links
 
 
 def extract_links_to_file(file_name):
     """
-    extract links to .txt file
-    :param file_name: output filename
-    :return: nothing
+    To write down links into .txt file
+    :param file_name: output filename / FILE_NAME in the same dir
+    :return: void
     """
-    kaggle_driver = connect()
-    competition_links = get_links_from_site(kaggle_driver)
-    output_comp_links = open(file_name, 'w')
-    for link in competition_links:
-        output_comp_links.write(link + '\n')
-    output_comp_links.close()
+    # create a driver
+    recipe_driver = connect()
+
+    # collect links
+    recipe_links = get_links_from_site(recipe_driver)
+
+    # write down links to the txt file
+    output_recipe_links = open(file_name, 'w')
+    for link in recipe_links:
+        output_recipe_links.write(link + '\n')
+    output_recipe_links.close()
+
+    print(f'links are in {file_name} file')
 
 
 if __name__ == '__main__':
-
-    links_file ='kaggle_links.txt'
+    links_file = FILE_NAME
     extract_links_to_file(links_file)
-
+    print('links are collected, the program is finished')
